75d781007c555c810a63aabdbfd21be8dc291f6a
Dave Kim <dave@goinstant.com>
2013-01-28 05:40:50 -0800

    new-issue.js

---
+++ new-issue.js
@@ -0,0 +1,19 @@
+// javascript:(function(a){var b=a.createElement("script");b.src="https://raw.github.com/YoDK/Bookmarklets/master/new-issue.js";a.getElementsByTagName("head")[0].appendChild(b)})(document);)
+var fieldValueMap = {
+  '#issue_title' : 'CATEGORY - Brief Description',
+  '.js-comment-field.js-size-to-fit': "###### Description" +
+    "\n\n* " +
+    "\n\n###### Reproduction Steps" +
+    "\n\n* " +
+    "\n\n###### Potential Causes" +
+    "\n\n* " +
+    "\n\n###### Console Output" +
+    "\n\n* " +
+    "\n\n###### Error Reports"+
+    "\n\n* "
+};
+
+
+Object.keys( fieldValueMap ).forEach(function( name ){
+  $(name).attr('value',fieldValueMap[name]);
+});
