6c3198e18906eab38ed9bce5e585966151fd4de0
Dave Kim <dave@goinstant.com>
2013-01-28 10:50:52 -0400

    Add minimum bar QA checklist

--- new-issue.js
+++ new-issue.js
@@ -10,7 +10,46 @@ var fieldValueMap = {
     "\n\n###### Console Output" +
     "\n\n* " +
     "\n\n###### Error Reports"+
-    "\n\n* "
+    "\n\n* "+
+    "\n\n#### Jenkins Results"+
+    "\n\n - [ ] MASTER"+
+    "\n\n - [ ] INSTANT"+
+    "\n\n*"+
+    "\n\n----"+
+    "\n\n*"+
+    "\n\n - [ ] What is the DIRECT ISSUE?"+
+    "\n\n - [ ] Any other issues related?"+
+    "\n\n - [ ] Any additional repros to check?"+
+    "\n\n*"+
+    "\n\n----"+
+    "\n\n*"+
+    "\n\n - [ ] Via https://www.gotesters.com/dev/API-Test-Page/ > Successfully start an API session"+
+    "\n\n - [ ] The API session is still logged into gotesters"+
+    "\n\n - [ ] Create and join a GUEST to the API session (has guest privileges)"+
+    "\n\n - [ ] Create and join an OWNER to the API session (has owner privileges)"+
+    "\n\n - [ ] Create and join a GUESTROLE to the API session (has guest privileges)"+
+    "\n\n - [ ] Create and join a OWNERROLE to the API session (has owner privileges)"+
+    "\n\n - [ ] Change the location via GAB to a RESTRICTED DOMAIN in the API session"+
+    "\n\n*"+
+    "\n\n----"+
+    "\n\n*"+
+    "\n\n * Connection tests via test.[server].goinstant.org"+
+    "\n\n - [ ] Safari OSX"+
+    "\n\n - [ ] Firefox OSX"+
+    "\n\n - [ ] Chrome OSX"+
+    "\n\n - [ ] IE8 XP"+
+    "\n\n - [ ] IE8 VISTA"+
+    "\n\n - [ ] IE9 W7"+
+    "\n\n - [ ] Firefox W7"+
+    "\n\n - [ ] Chrome W7"+
+    "\n\n*"+
+    "\n\n----"+
+    "\n\n*"+
+    "\n\n#### IDE"+
+    "\n\n  - [ ]  __LiveTests-Supported-Part-01 - Tests 11 / Expected 11"+
+    "\n\n  - [ ]  __LiveTests-Supported-Part-02 - Tests 11 / Expected 11"+
+    "\n\n - [ ]  __GoTesters-Supported - Tests 40 / Expected 40"+
+    "\n\n  - [ ]  __Dashboard - Tests 16 / Expected 16"
 };
 
 
