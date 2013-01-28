52930dd3ad7b7ab4ca1a4f07ee3b45b3e0568166
Dave Kim <dave@goinstant.com>
2013-01-28 19:10:08 -0400

    Better format

--- new-pull-request.js
+++ new-pull-request.js
@@ -8,39 +8,83 @@ var fieldValueMap = {
     "\n\n###### Notes for QA" +
     "\n\n* "
     "\n\n----"+
-    "\n\n#### Jenkins Results"+
-    "\n\n - [ ] [ ] MASTER"+
-    "\n\n - [ ] [ ] INSTANT"+
+     "\n\n#### Jenkins Results"+
+    "\n\n - [ ] MASTER"+
+    "\n\n - [ ] INSTANT"+
     "\n\n----"+
     "\n\n#### Issues"+
-    "\n\n - [ ] [ ] What is the DIRECT ISSUE?"+
-    "\n\n - [ ] [ ] Any other issues related?"+
-    "\n\n - [ ] [ ] Any additional repros to check?"+
+    "\n\n##### What is the DIRECT ISSUE?"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Any other issues related?"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Any additional repros to check?"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
     "\n\n----"+
     "\n\n#### API"+
-    "\n\n - [ ] [ ] Via https://www.gotesters.com/dev/API-Test-Page/ > Successfully start an API session"+
-    "\n\n - [ ] [ ] The API session is still logged into gotesters"+
-    "\n\n - [ ] [ ] Create and join a GUEST to the API session (has guest privileges)"+
-    "\n\n - [ ] [ ] Create and join an OWNER to the API session (has owner privileges)"+
-    "\n\n - [ ] [ ] Create and join a GUESTROLE to the API session (has guest privileges)"+
-    "\n\n - [ ] [ ] Create and join a OWNERROLE to the API session (has owner privileges)"+
-    "\n\n - [ ] [ ] Change the location via GAB to a RESTRICTED DOMAIN in the API session"+
+    "\n\n##### Via https://www.gotesters.com/dev/API-Test-Page/ > Successfully start an API session"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### The API session is still logged into gotesters"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Create and join a GUEST to the API session (has guest privileges)"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Create and join an OWNER to the API session (has owner privileges)"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Create and join a GUESTROLE to the API session (has guest privileges)"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Create and join a OWNERROLE to the API session (has owner privileges)"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Change the location via GAB to a RESTRICTED DOMAIN in the API session"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
     "\n\n----"+
     "\n\n#### Connection via test.[server]"+
-    "\n\n - [ ] [ ] Safari OSX"+
-    "\n\n - [ ] [ ] Firefox OSX"+
-    "\n\n - [ ] [ ] Chrome OSX"+
-    "\n\n - [ ] [ ] IE8 XP"+
-    "\n\n - [ ] [ ] IE8 VISTA"+
-    "\n\n - [ ] [ ] IE9 W7"+
-    "\n\n - [ ] [ ] Firefox W7"+
-    "\n\n - [ ] [ ] Chrome W7"+
+    "\n\n##### Safari OSX"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Firefox OSX"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### Chrome OSX"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### IE8 XP"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### IE8 VISTA"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### IE9 W7"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n#####  Firefox W7"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n#####  Chrome W7"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
     "\n\n----"+
     "\n\n#### IDE"+
-    "\n\n  - [ ] [ ] __LiveTests-Supported-Part-01 - Tests 11 / Expected 11"+
-    "\n\n  - [ ] [ ] __LiveTests-Supported-Part-02 - Tests 11 / Expected 11"+
-    "\n\n  - [ ] [ ] __GoTesters-Supported - Tests 40 / Expected 40"+
-    "\n\n  - [ ] [ ] __Dashboard - Tests 16 / Expected 16"
+    "\n\n##### __LiveTests-Supported-Part-01 - Tests 11 / Expected 11"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### __LiveTests-Supported-Part-02 - Tests 11 / Expected 11"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### __GoTesters-Supported - Tests 40 / Expected 40"+
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
+    "\n\n##### __Dashboard - Tests 16 / Expected 16"
+    "\n\n - [ ] Dev
+    "\n\n - [ ] QA
 };
 
 
