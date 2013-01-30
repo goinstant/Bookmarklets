840a3a9ce6aece88f5ae4daa67299fd2f8c2a43b
Dave Kim <dave@goinstant.com>
2013-01-30 10:56:57 -0400

    Update new-pull-request.js

--- new-pull-request.js
+++ new-pull-request.js
@@ -12,78 +12,54 @@ var fieldValueMap = {
     "\n\n - [ ] MASTER"+
     "\n\n - [ ] INSTANT"+
     "\n\n----"+
-    "\n\n#### Issues"+
-    "\n\n##### What is the DIRECT ISSUE?"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Any other issues related?"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Any additional repros to check?"+
+    "\n\n#### Direct Issue"+
     "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n----"+
     "\n\n#### API"+
-    "\n\n##### Via https://www.gotesters.com/dev/API-Test-Page/ > Successfully start an API session"+
-    "\n\n - [ ] Dev"+
+    "\n\n##### Via https://www.gotesters.com/dev/API-Test-Page"+
     "\n\n - [ ] QA"+
-    "\n\n##### The API session is still logged into gotesters"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a GUEST to the API session (has guest privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join an OWNER to the API session (has owner privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a GUESTROLE to the API session (has guest privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a OWNERROLE to the API session (has owner privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Change the location via GAB to a RESTRICTED DOMAIN in the API session"+
-    "\n\n - [ ] Dev"+
+    "\n\n##### Via http://demo1.nevo.com/jhltc/GoInstantRevisedPOC1892603/"+
     "\n\n - [ ] QA"+
     "\n\n----"+
-    "\n\n#### Connection via test.server"+
+    "\n\n#### Window.open"+
+    "\n\n - [ ] QA"+
+    "\n\n----"+
+    "\n\n#### PDFs"+
+    "\n\n##### IE8"+
+    "\n\n - [ ] QA"+
+    "\n\n##### Dynamic"+
+    "\n\n - [ ] QA"+
+    "\n\n----"+
+    "\n\n#### Field Masking"+
+    "\n\n - [ ] QA"+
+    "\n\n#### Field Locking"+
+    "\n\n - [ ] QA"+
+    "\n\n----"+
+    "\n\n#### Connection tests via test.server"+
     "\n\n##### Safari OSX"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n##### Firefox OSX"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n##### Chrome OSX"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n##### IE8 XP"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n##### IE8 VISTA"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n##### IE9 W7"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n#####  Firefox W7"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n#####  Chrome W7"+
-    "\n\n - [ ] Dev"+
     "\n\n - [ ] QA"+
     "\n\n----"+
     "\n\n#### IDE"+
-    "\n\n##### LiveTests-Supported-Part-01 - Tests 11 / Expected 11"+
-    "\n\n - [ ] Dev"+
+    "\n\n##### Live Tests"+
     "\n\n - [ ] QA"+
-    "\n\n##### LiveTests-Supported-Part-02 - Tests 11 / Expected 11"+
-    "\n\n - [ ] Dev"+
+    "\n\n##### Control Tests"+
     "\n\n - [ ] QA"+
-    "\n\n##### GoTesters-Supported - Tests 40 / Expected 40"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Dashboard - Tests 16 / Expected 16"+
-    "\n\n - [ ] Dev"+
+    "\n\n##### Dashboard Tests"+
     "\n\n - [ ] QA"
     
 };
