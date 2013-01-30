334f25410c16adac85d0d81681c186a7e9a5a2f6
Dave Kim <dave@goinstant.com>
2013-01-30 10:49:56 -0400

    Update new-issue.js

--- new-issue.js
+++ new-issue.js
@@ -12,83 +12,20 @@ var fieldValueMap = {
     "\n\n###### Error Reports"+
     "\n\n* " +
     "\n\n----"+
-    "\n\n#### Jenkins Results"+
-    "\n\n - [ ] MASTER"+
-    "\n\n - [ ] INSTANT"+
-    "\n\n----"+
-    "\n\n#### Issues"+
-    "\n\n##### What is the DIRECT ISSUE?"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Any other issues related?"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Any additional repros to check?"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n----"+
-    "\n\n#### API"+
-    "\n\n##### Via https://www.gotesters.com/dev/API-Test-Page/ > Successfully start an API session"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### The API session is still logged into gotesters"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a GUEST to the API session (has guest privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join an OWNER to the API session (has owner privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a GUESTROLE to the API session (has guest privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Create and join a OWNERROLE to the API session (has owner privileges)"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Change the location via GAB to a RESTRICTED DOMAIN in the API session"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n----"+
-    "\n\n#### Connection via test.server"+
-    "\n\n##### Safari OSX"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Firefox OSX"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Chrome OSX"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### IE8 XP"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### IE8 VISTA"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### IE9 W7"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n#####  Firefox W7"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n#####  Chrome W7"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n----"+
-    "\n\n#### IDE"+
-    "\n\n##### LiveTests-Supported-Part-01 - Tests 11 / Expected 11"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### LiveTests-Supported-Part-02 - Tests 11 / Expected 11"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### GoTesters-Supported - Tests 40 / Expected 40"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"+
-    "\n\n##### Dashboard - Tests 16 / Expected 16"+
-    "\n\n - [ ] Dev"+
-    "\n\n - [ ] QA"
+    "\n\n#### Discover"+
+    "\n\n - [ ] Reproduce the bug. Document the reproduction steps in the issue (if they aren't there already)."+
+    "\n\n#### Categorize"+
+    "\n\n - [ ] Determine the underlying technology that we're not compatible with. Add an issue to support the underlying technology (if it isn't there already) and then link this issue to it."+
+    "\n\n##### GoTesters"+
+    "\n\n - [ ] Recreate bug on GoTesters: Document the addition by adding a description of how it should behave and a link to the issue."+
+    "\n\n##### GoScanner"+
+    "\n\n - [ ] Add a test to the compatibility scanner tool."+
+    "\n\n##### Fix"+
+    "\n\n - [ ] Fix the problem. Ideally, you can add compatibility for the underlying technology :), but you may have to write an exception :(. This is a time-benefit analysis. How long will it take you to fix the underlying technology? vs. how long will an exception take?"+
+    "\n\n##### Unit Test"+
+    "\n\n - [ ] Write unit tests and integration tests, if necessary"+
+    "\n\n##### Use Pull Request Bookmarklet to auto populate QA checklist"+
+    "\n\n - [ ] Basic preliminary testing"
 
 };
 
