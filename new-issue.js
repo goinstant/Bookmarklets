a7e01de6c0121c9f73f26a7a9f0cd91fa5bfca30
Jeremy Stashewsky <jstash@gmail.com>
2013-03-07 11:00:30 -0800

    Use #issue_body for template text

--- new-issue.js
+++ new-issue.js
@@ -1,7 +1,8 @@
-// javascript:(function(a){var b=a.createElement("script");b.src="https://raw.github.com/adamwdennis/Bookmarklets/master/githubNewIssueTemplate.js";a.getElementsByTagName("head")[0].appendChild(b)})(document);)
+// javascript:(function(a){var b=a.createElement("script");b.src="https://raw.github.com/goinstant/Bookmarklets/master/githubNewIssueTemplate.js";a.getElementsByTagName("head")[0].appendChild(b)})(document);)
 var fieldValueMap = {
   '#issue_title' : 'CATEGORY - Brief Description',
-  '.js-comment-field.js-size-to-fit': "###### Description" +
+  '#issue_body': 
+    "###### Description" +
     "\n\n* " +
     "\n\n###### Reproduction Steps" +
     "\n\n* " +
